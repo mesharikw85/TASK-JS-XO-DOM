5261fa1abf65e30a8c2e8f269b2d2bb5ac13ead2
mesharikw85 <mkwg85@gmail.com>
2023-05-25 20:37:59 +0300

    done task x o

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ function fillButton(index, text) {
 }
 // pre-made a function. You can use this function to present an alert to say someone wins
 function winningAlert(winner) {
-  if(confirm(`Horraaay, ${winner} wins!`)){
-     // The code here will be exectued if you press on OK button that will pop on the window 
+  if (confirm(`Horraaay, ${winner} wins!`)) {
+    // The code here will be exectued if you press on OK button that will pop on the window
   }
 }
 
 // SAMPLE CODE: This code fills the 1st and 9th button with X and O initially
 // ❗️ Delete this code once you are done testing
-fillButton(1, "X");
-fillButton(9, "O");
+//fillButton(1, "X");
+//fillButton(9, "O");
 
 /**
  *
@@ -24,9 +24,58 @@ fillButton(9, "O");
  * Add your code here, since this is going to be your main function
  * That interacts with the UI
  */
+// Define the game board
+let gameBoard = ["", "", "", "", "", "", "", "", ""];
+// Define the current player
+let currentPlayer = "X";
+
+// This function is called when any button is clicked in the game
 function clickButton(index) {
-  console.log(`Button number ${index} is clicked`);
+  console.log(`Button number ${index} is clicked`); // Display a message containing the button number that was clicked
   // Your main code here.
+
+  if (gameBoard[index - 1] === "") {
+    // Check if the clicked button has not been clicked before
+    gameBoard[index - 1] = currentPlayer; // Assign the current player to the clicked button
+    fillButton(index, currentPlayer); // Fill the clicked button with the current player's symbol "X" or "O"
+    if (checkWinner(currentPlayer)) {
+      // Check if there is a winner in the game
+      winningAlert(currentPlayer); // Display a message indicating the current player won
+      resetGame(); // Reset the game after a win
+    } else {
+      currentPlayer = currentPlayer === "X" ? "O" : "X"; // Switch the current player between "X" and "O"
+    }
+  } else {
+    alert("This square is already taken!"); // Display a warning message if the clicked button is already taken
+  }
+}
+
+// This function checks if there is a winner in the game
+function checkWinner(player) {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // Check if any of the winning combinations exists in the game board
+  return winningCombinations.some((combination) =>
+    combination.every((index) => gameBoard[index] === player)
+  );
+}
+
+// This function resets the game board and current player to their default values
+function resetGame() {
+  gameBoard = ["", "", "", "", "", "", "", "", ""];
+  for (let i = 1; i <= 9; i++) {
+    fillButton(i, ""); // Clear all buttons text
+  }
+  currentPlayer = "X"; //Reset current player to "X"
 }
 
 /**
